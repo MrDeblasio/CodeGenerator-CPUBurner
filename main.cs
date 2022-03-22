3c2b17ed3d4cf06ac34acd07a03fbfd92a1275e9
Deblasio <43344427+MrDeblasio@users.noreply.github.com>
2022-03-22 23:09:11 +0400

    Fixed the code so that it can actually end

--- main.cs
+++ main.cs
@@ -2,40 +2,27 @@ using System;
 using System.Threading;
 using System.Collections.Generic;
 
-class Program
+namespace Crackerjack
 {
-
-	static bool isrunning = true;
-
-	static string password = "ljp";
-	static string word = "";
-	const int intlimit = 2147480000;
-	static int limitsreached = 0;
-	static int attempts = 0;
-	static int wordlength = 3;
-	static Random rand = new Random();
-	static string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-	// static string chars = "1234567890";
-
-	static DateTime StartingTime = DateTime.Now;
-	static DateTime EndingTime;
-
-	static void GeneratePassword()
+	class Program
 	{
-		string tempword = "";
-		for (int i = 0; i < wordlength; i++)
-		{
-			int randint = rand.Next(0, chars.Length);
-			// Console.WriteLine(chars[randint]);
-			tempword += chars[randint];
-		}
-		Console.WriteLine($"Password generated is: {tempword}");
-		password = tempword;
-	}
 
-	static void FindPassword()
-	{
-		while (isrunning)
+		static bool isrunning = true;
+
+		static string password = "hacks";
+		static string word = "";
+		const int intlimit = 2147480000;
+		static int limitsreached = 0;
+		static int attempts = 0;
+		static int wordlength = 5;
+		static Random rand = new Random();
+		static string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+		// static string chars = "1234567890";
+
+		static DateTime StartingTime = DateTime.Now;
+		static DateTime EndingTime;
+
+		static void GeneratePassword()
 		{
 			string tempword = "";
 			for (int i = 0; i < wordlength; i++)
@@ -44,50 +31,67 @@ class Program
 				// Console.WriteLine(chars[randint]);
 				tempword += chars[randint];
 			}
-
-			word = tempword;
-
-			if (attempts >= intlimit)
-			{
-				limitsreached++;
-				// Console.WriteLine($"Integer limit reached, total: {limitsreached}");
-				attempts = 0;
-			}
-			else
-			{
-				attempts++;
-				Console.WriteLine($"{attempts}, {limitsreached}, {tempword}");
-			}
-			// Console.WriteLine($"{tempword}");
-
-			if (password == tempword && isrunning == false)
-			{
-				EndingTime = DateTime.Now;
-				// Attempt count: {attempts + (intlimit*limitsreached)}\n\
-				Console.WriteLine($"Found word! Word: {tempword}\n\nStartingTime: {StartingTime}\nEndingTime: {EndingTime}\nTimeDifference: {EndingTime.Subtract(StartingTime).TotalMinutes}");
-				isrunning = false;
-			}
+			Console.WriteLine($"Password generated is: {tempword}");
+			password = tempword;
 		}
-	}
 
-	public static void Main(string[] args)
-	{
-		// GeneratePassword();
-
-		List<Thread> Threads = new List<Thread>();
-
-		for (int i = 0; i < 1000; i++)
+		static void FindPassword()
 		{
-			Thread t = new Thread(FindPassword);
-			Threads.Add(t);
+			while (isrunning)
+			{
+				string tempword = "";
+				for (int i = 0; i < wordlength; i++)
+				{
+					int randint = rand.Next(0, chars.Length);
+					// Console.WriteLine(chars[randint]);
+					tempword += chars[randint];
+				}
+
+				word = tempword;
+
+				if (attempts >= intlimit)
+				{
+					limitsreached++;
+					// Console.WriteLine($"Integer limit reached, total: {limitsreached}");
+					attempts = 0;
+				}
+				else
+				{
+					attempts++;
+					Console.WriteLine($"{attempts}, {limitsreached}, {tempword}");
+				}
+				// Console.WriteLine($"{tempword}");
+
+				if (password == tempword && isrunning == true)
+				{
+					EndingTime = DateTime.Now;
+					// Attempt count: {attempts + (intlimit*limitsreached)}\n\
+					Console.WriteLine($"Found word! Word: {tempword}\n\nStartingTime: {StartingTime}\nEndingTime: {EndingTime}\nTimeDifference: {EndingTime.Subtract(StartingTime).TotalMinutes}");
+					isrunning = false;
+					break;
+				}
+			}
 		}
 
-		foreach (Thread t in Threads)
+		public static void Main(string[] args)
 		{
-			t.Start();
-			t.Join();
-		}
+			// GeneratePassword();
 
-		Console.WriteLine($"All threads finished.");
+			List<Thread> Threads = new List<Thread>();
+
+			for (int i = 0; i < 1000; i++)
+			{
+				Thread t = new Thread(FindPassword);
+				Threads.Add(t);
+			}
+
+			foreach (Thread t in Threads)
+			{
+				t.Start();
+				t.Join();
+			}
+
+			Console.WriteLine($"All threads finished.");
+		}
 	}
 }
